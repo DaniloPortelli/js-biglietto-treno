31ea09094cc2e0525899131d7912503c93eb23af
DaniloPortelli <daniloportelli@gmail.com>
2025-01-09 16:51:42 +0100

    invio esercizio

--- main.js
+++ main.js
@@ -1,20 +1,24 @@
 // Chilometri
-const chilometri = 27;
+let chilometri = prompt("Inserisci la distanza da percorrere");
 console.log(`Chilometri da percorrere: ${chilometri}`);
 
 
-
 // Età
-const età = 65;
-console.log(`Età del passeggero: ${età}`)
+let eta = prompt("Inserisci la tua età");
+console.log(`Età del passeggero: ${eta}`)
 
-
-
+let prezzo = chilometri * 0.21;
+let sconto = 0;
 // Biglietto
 
-if (età >= 65){
-    console.log(`Il prezzo del biglietto è di € ${chilometri* 0.21} `)
+if (eta >= 65){
+    sconto = prezzo * 0.4;
+} else if (eta < 18){
+    sconto = prezzo * 0.2;
 }
+   
+prezzo = prezzo - sconto;
+console.log(`Il prezzo del biglietto è di € ${prezzo.toFixed(2)}`)
 
 
 
